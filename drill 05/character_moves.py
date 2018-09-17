dfe67838dd234045c7a2022c0e0f18f3e0f215d1
Lee Yeajun <lyj12100@naver.com>
2018-09-17 21:57:08 +0900

    첫번째점에서 두번째 점으로 이동

--- drill 05/character_moves.py
+++ drill 05/character_moves.py
@@ -5,7 +5,7 @@ open_canvas()
 grass=load_image('grass.png')
 character=load_image('animation_sheet.png')
 
-def from_first_to_second():
+def firstpos():
     frame=0
     count=0
     x,y=203,535
@@ -21,6 +21,26 @@ def from_first_to_second():
         frame=(frame+1)%8
         delay(0.05)
 
-from_first_to_second()
+def go_secondpos():
+    frame = 0
+    count = 0
+    x, y = 132, 243
+    nextposx, nextposy = (535 - 132) / 50, (470 - 243) / 50
+    while count < 50:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        count += 1
+        x += nextposx
+        y += nextposy
+        frame = (frame + 1) % 8
+        delay(0.05)
+
+
+
+while True:
+    firstpos()
+    go_secondpos()
 
 close_canvas()
